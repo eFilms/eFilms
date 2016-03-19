c6d489184944efd50c04fc9f211bc36f772154b3
Chris Brown <chris@ourbigadventure.com>
2016-03-19 17:31:23 -0400

    updated require paths

--- _ajax/eFEditorVResourcesContentNewPopDown.php
+++ _ajax/eFEditorVResourcesContentNewPopDown.php
@@ -9,8 +9,8 @@ if ($_SESSION["login"] != "true"){
   exit;
 }
 
-require_once('/settings.php');
-require_once('/includes/functions.php');
+require_once('settings.php');
+require_once('includes/functions.php');
 require_once(directoryAboveWebRoot().'/db_con.php');
 
 $eFResL2ContPopDownSearch = (isset($_GET['eFResL2ContPopDownSearch']) ? $_GET['eFResL2ContPopDownSearch'] : "");
