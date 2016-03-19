37184d3d0438eb46f985ae0e6783578179e53137
Chris Brown <chris@ourbigadventure.com>
2016-03-19 17:33:58 -0400

    updated require paths

--- _ajax/eFEditorVResourcesInMovie.php
+++ _ajax/eFEditorVResourcesInMovie.php
@@ -9,8 +9,8 @@ if ($_SESSION["login"] != "true"){
   exit;
 }
 
-require_once('/settings.php');
-require_once('/includes/functions.php');
+require_once('settings.php');
+require_once('includes/functions.php');
 require_once(directoryAboveWebRoot().'/db_con.php');
 
 $uniqueUTS = time();
